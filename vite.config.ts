1701287249213e7a0a5e7f50095974ff6d1a17aa
FriedAxons <friedaxons@proton.me>
2024-11-08 15:45:11 -0600

    Edit vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/frontend-mentor-todo-app/", // Set the base to match your repository name
   plugins: [react()], // React plugin, still needed for your project
+  base: "/frontend-mentor-todo-app/", // Set the base to match your repository name
   build: {
     outDir: "dist", // Vite will output the build files to the 'dist' folder
   },
