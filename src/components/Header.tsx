85992c2dcf29b4ed376327f784d9d7172771a19c
FriedAxons <friedaxons@proton.me>
2024-11-08 22:30:55 -0600

    Test changes for styling fix

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,11 +10,7 @@ const Header: React.FC<HeaderProps> = ({ darkMode, toggleDarkMode }) => {
     <header className={styles.header}>
       <h1 className={styles.title}>Todo</h1>
       <img
-        src={
-          darkMode
-            ? "../../public/images/icon-moon.svg"
-            : "../../public/images/icon-sun.svg"
-        }
+        src={darkMode ? "/images/icon-moon.svg" : "/images/icon-sun.svg"}
         alt={darkMode ? "Moon icon" : "Sun icon"}
         className={styles.icon}
         onClick={toggleDarkMode}
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()], // React plugin, still needed for your project
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": "/src", // Add alias for src folder
+    },
+  },
   css: {
     preprocessorOptions: {
       scss: {
-        additionalData: `@use "src/styles/variables.scss" as *;`,
-        api: "modern", // or 'modern-compiler'
+        additionalData: `@use "@/styles/variables.scss" as *;`, // Corrected import
+        api: "modern", // Optional: Using the modern API
       },
     },
   },
