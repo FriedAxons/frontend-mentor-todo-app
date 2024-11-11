a904d9c7e5a94cf0a199765be177f1d2b95f54fe
FriedAxons <friedaxons@proton.me>
2024-11-10 23:40:08 -0600

    Commit styling progress

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,7 +10,7 @@ const Header: React.FC<HeaderProps> = ({ darkMode, toggleDarkMode }) => {
     <header className={styles.header}>
       <h1 className={styles.h1}>TODO</h1>
       <img
-        src={darkMode ? "images/icon-moon.svg" : "images/icon-sun.svg"} // Dynamic image source based on darkMode "
+        src={darkMode ? "images/icon-sun.svg" : "images/icon-moon.svg"} // Dynamic image source based on darkMode "
         alt={darkMode ? "Moon icon" : "Sun icon"}
         className={styles.img}
         onClick={toggleDarkMode} // Calls toggleTheme in App on click
