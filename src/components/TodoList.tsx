bc943f543ae842a5abd0823ba98567945a59db78
FriedAxons <friedaxons@proton.me>
2024-11-13 20:26:07 -0600

    Commit project styles

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -92,7 +92,7 @@ const TodoList: React.FC<TodoListProps> = ({
           Clear Completed
         </button>
       </div>
-      <p className="dragHint">Drag and drop to reorder list</p>
+      <p className={styles.dragHint}>Drag and drop to reorder list</p>
     </div>
   );
 };
